4f0c75a5213212019829f195db6238ad4b0e489d
Geert Vanderkelen <geert@vanderkelen.org>
2022-11-06 18:30:19 +0100

    Use generics for supporting string types

--- slicing.go
+++ slicing.go
@@ -7,14 +7,15 @@ import "strings"
 // RepeatSlice returns a []string consisting of count copies of the string s.
 // When count is zero, empty slice is returned.
 // Panics when count is out of range when making slice.
-func RepeatSlice(s string, count int) []string {
+func RepeatSlice[T ~string](s T, count int) []string {
 	if count == 0 {
 		return []string{}
 	}
 
 	n := make([]string, count)
+	rs := string(s)
 	for i := 0; i < count; i++ {
-		n[i] = s
+		n[i] = rs
 	}
 	return n
 }
@@ -23,12 +24,12 @@ func RepeatSlice(s string, count int) []string {
 // concatenated using the separate string sep.
 // This is similar as using RepeatSlice together with Go's strings.Join.
 // Panics when count is negative.
-func RepeatJoin(s string, count int, sep string) string {
+func RepeatJoin[T ~string](s T, count int, sep string) string {
 	switch {
 	case count == 0:
 		return ""
 	case count == 1:
-		return s
+		return string(s)
 	case count < 0:
 		panic("xstrings: negative RepeatJoin count")
 	}
@@ -37,11 +38,11 @@ func RepeatJoin(s string, count int, sep string) string {
 
 	var b strings.Builder
 	b.Grow(n)
-	b.WriteString(s)
+	b.WriteString(string(s))
 
 	for i := 1; i < count; i++ {
 		b.WriteString(sep)
-		b.WriteString(s)
+		b.WriteString(string(s))
 	}
 
 	return b.String()
